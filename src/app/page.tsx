76e6fa3180aaa855343ebb2b23ce699d5fcf4b77
Leonardo1596 <leonardoemanuel156@gmail.com>
2025-10-15 01:55:25 -0300

    adjusted many things in transactions page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,107 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Logo } from '@/components/logo';
-import { useEffect, useState } from 'react';
-import { login } from '../services/auth';
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Logo } from "@/components/logo";
+import { useEffect, useState } from "react";
+import { login } from "../services/auth";
 
 export default function LoginPage() {
-  const router = useRouter();
+	const router = useRouter();
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedUser = localStorage.getItem('user');
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			const storedUser = localStorage.getItem("user");
 
-      if (storedUser) {
-        router.push('/dashboard');
-      }
-    }
-  }, []);
-  
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+			if (storedUser) {
+				router.push("/dashboard");
+			}
+		}
+	}, []);
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const user = await login(email, password);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-      router.push('/dashboard');
-    } catch (err) {
+	const handleLogin = async (e: React.FormEvent) => {
+		e.preventDefault();
+		try {
+			const user = await login(email, password);
 
-    }
-  };
+			router.push("/dashboard");
+		} catch (err) {}
+	};
 
-  return (
-    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div className="grid gap-2 text-center">
-            <Logo />
-            <h1 className="text-3xl font-bold font-headline mt-4">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Entre com seu email para acessar o painel
-            </p>
-          </div>
-          <form onSubmit={handleLogin} className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Senha</Label>
-                <Link
-                  href="#"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Esqueceu sua senha?
-                </Link>
-              </div>
-              <Input id="password" type="password" placeholder='senha' required value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-          </form>
-          <div className="mt-4 text-center text-sm">
-            Não tem uma conta?{' '}
-            <Link href="/register" className="underline">
-              Cadastre-se
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="https://images.unsplash.com/photo-1672257493395-c6cb634397e2?q=80&w=1962&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/1920/1080"
-          alt="Barbershop"
-          data-ai-hint="barbershop interior"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.3]"
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
+			<div className="flex items-center justify-center py-12">
+				<div className="mx-auto grid w-[350px] gap-6">
+					<div className="grid gap-2 text-center">
+						<Logo />
+						<h1 className="text-3xl font-bold font-headline mt-4">
+							Login
+						</h1>
+						<p className="text-balance text-muted-foreground">
+							Entre com seu email para acessar o painel
+						</p>
+					</div>
+					<form onSubmit={handleLogin} className="grid gap-4">
+						<div className="grid gap-2">
+							<Label htmlFor="email">Email</Label>
+							<Input
+								id="email"
+								type="email"
+								placeholder="m@example.com"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</div>
+						<div className="grid gap-2">
+							<div className="flex items-center">
+								<Label htmlFor="password">Senha</Label>
+								<Link
+									href="#"
+									className="ml-auto inline-block text-sm underline"
+								>
+									Esqueceu sua senha?
+								</Link>
+							</div>
+							<Input
+								id="password"
+								type="password"
+								placeholder="senha"
+								required
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
+						<Button type="submit" className="w-full">
+							Login
+						</Button>
+					</form>
+					<div className="mt-4 text-center text-sm">
+						Não tem uma conta?{" "}
+						<Link href="/register" className="underline">
+							Cadastre-se
+						</Link>
+					</div>
+				</div>
+			</div>
+			<div className="hidden bg-muted lg:block">
+				<Image
+					src="https://images.unsplash.com/photo-1672257493395-c6cb634397e2?q=80&w=1962&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/1920/1080"
+					alt="Barbershop"
+					data-ai-hint="barbershop interior"
+					width="1920"
+					height="1080"
+					className="h-full w-full object-cover dark:brightness-[0.3]"
+					priority
+				/>
+			</div>
+		</div>
+	);
 }
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,74 +1,82 @@
 export type User = {
-  id: string;
-  _id?: string;
-  name: string;
-  email: string;
-  role: 'admin' | 'manager';
-  barbershopId?: string;
+	id: string;
+	_id?: string;
+	name: string;
+	email: string;
+	role: "admin" | "manager";
+	barbershopId?: string;
 };
 
 export type Barbershop = {
-  id: string;
-  _id?: string;
-  name: string;
+	id: string;
+	_id?: string;
+	name: string;
 };
 
 export type Barber = {
-  id: string;
-  _id?: string;
-  name: string;
-  barbershopId: string;
+	id: string;
+	_id?: string;
+	name: string;
+	barbershopId: string;
 };
 
 export type Service = {
-  id: string;
-  _id?: string;
-  name: string;
-  price: number;
-  duration: number; // in minutes
-  barbershopId: string;
+	id: string;
+	_id?: string;
+	name: string;
+	price: number;
+	duration: number; // in minutes
+	barbershopId: string;
 };
 
 export type Product = {
-  id: string;
-  _id?: string;
-  name: string;
-  stock: number;
-  price: number;
-  quantity: number;
-  description: string;
-  barbershop: string;
+	id: string;
+	_id?: string;
+	name: string;
+	stock: number;
+	amount: number;
+	date: string;
+	quantity: number;
+	description: string;
+	barbershop: string;
+	barber: string;
 };
 
 export type Appointment = {
-  id: string;
-  _id?: string;
-  clientName: string;
-  barbershop: string;
-  barber: string;
-  services: string[];
-  date: string;
-  hour: string;
-  paymentMethod: 'dinheiro' | 'cartao' | 'pix';
-  status: 'agendado' | 'concluido' | 'cancelado';
+	id: string;
+	_id?: string;
+	clientName: string;
+	barbershop: string;
+	barber: string;
+	services: string[];
+	date: string;
+	hour: string;
+	amount: number;
+	paymentMethod: "dinheiro" | "cartao" | "pix";
+	status: "agendado" | "concluido" | "cancelado";
 };
 
-export type TransactionDetails = 
-  | { kind: 'appointment', appointmentId: string }
-  | { kind: 'product_sale', productId: string, quantity: number, barberId: string }
-  | { kind: 'general' };
+export type TransactionDetails =
+	| { kind: "appointment"; appointmentId: string }
+	| {
+			kind: "product_sale";
+			productId: string;
+			quantity: number;
+			barberId: string;
+	  }
+	| { kind: "general" };
 
 export type Transaction = {
-  id: string;
-  _id?: string;
-  barbershopId: string;
-  userId: string;
-  type: 'revenue' | 'expense';
-  amount: number;
-  description: string;
-  date: string;
-  status: 'Pendente' | 'Completo' | 'Falhou';
-  details: TransactionDetails;
+	id: string;
+	_id?: string;
+	barbershopId: string;
+	userId: string;
+	type: "revenue" | "expense";
+	amount: number;
+	description: string;
+	date: string;
+	status: "Pendente" | "Completo" | "Falhou";
+	details: TransactionDetails;
 };
 
 export type Report = {
